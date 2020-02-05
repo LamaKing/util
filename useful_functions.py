e78dddc7288f409399f4eb1e085a0769bc6d1ff2
Andrea Silva <andreasilva759@gmail.com>
2020-02-05 12:58:44 +0000

    Merge with local other utilities

--- useful_functions.py
+++ useful_functions.py
@@ -1,26 +1,26 @@
+
 """
 --------------------------------
- GENERAL-PORPOUSE FUNCTIONS
+ GENERAL PORPOUSE FUNCTIONS
 --------------------------------
 
-A collection of often-used functions:
- - code shortcuts;
- - smart file readers;
- - string formatter;
- - container manipulators;
-
+A pletora of general functions:
+ - small ones just to save time, aka code shortcuts;
+ - smart file readers (skip bash comments, divide in fields, ...);
+ - string formatter (fix width, ...);
+ - container manipulators (flatten nested strings)
 Python 3 only.
 
 Created:    Silva 29-01-2018
-Last edit:  Silva 03-11-2019
+Last edit:  Silva 04-04-2019
 """
 
 import collections
 import logging
 
-#------------------------------------------------------------------------------#
-# Bash-like script setup shortcuts
-#------------------------------------------------------------------------------#
+#--------------------------------
+# Set up LOGGER
+#--------------------------------
 
 def logger_setup(module_name):
     """Set up standar logger for a module and return it"""
@@ -33,12 +33,12 @@ def logger_setup(module_name):
     # Logging level is defined by calling module via root logger
     return c_log
 
-#------------------------------------------------------------------------------#
-# Shortcut
-#------------------------------------------------------------------------------#
+#--------------------------------
+# Short functions with heavly used combos
+#--------------------------------
 
-def list2str(listarg, sep=' '):
-    """Create a string out of a list with given separator (def=space)"""
+def strlist(listarg, sep=' '):
+    """Create a string out of a list with gien separator (def=space)"""
     sep = str(sep)
     return sep.join(lmap(str, listarg))
 
@@ -46,163 +46,128 @@ def lmap(func, listarg):
     """Map given function to given list and return a list instead of iterator"""
     return list(map(func, listarg))
 
-#------------------------------------------------------------------------------#
+#--------------------------------
 # Read files smartly
-#------------------------------------------------------------------------------#
+#--------------------------------
+
+def load_file_lines(filename):
+    """Try to open the given file and read the content.
+    Return the file as list of lines (each as string)
+    """
+    c_log = logger_setup(__name__)
+
+    filename = str(filename)
+    # Try to open file and report failures
+    try:
+        in_f = open(filename, 'r')
+    except:
+        c_log.error("Invalid file %s", filename)
+
+    lines = [l.strip() for l in in_f.readlines()]
+    in_f.close()
+    return lines
+
 # Load data (comments+data)
-# Should rename to split_stream
-def load_stream(stream, comment_char="#", f=lambda x: x, split=True):
-    """Load given stream object.
+def load_stream(stream, comment_char="#", f=lambda x: x):
+    """Load given stream object. Divide lines between comments and data.
+    Comment char can be given as input.
+    A function f can be given given as input and applied to each non-comment field."""
+    tmp_in = stream.readlines()
 
-    Divide lines between comments (default char=#) and data, each divided in its fields.
-
-    A function f can be given given as input and applied to each non-comment field.
-
-    Accoring to split flag:
-    - True (default): return tuple (comments, data)
-                      Comment lines l are return as "<comment_char><line number>:l".
-    - False: return tuple (coment indices, ordered field-split data and comment)
-
-    Not suitable for big files: all content loaded."""
-
-    stream_lines = stream.readlines()
-
-    # Read the input file and filter the bash-like comments
-    # Split comments and data in sparate objects
-    if split:
-        comments = [(comment_char+"%i:"%i) + l[1:].strip()
-                    for i, l in enumerate(stream_lines)
-                    if l.strip()[0] == comment_char]
-        data = [[f(x) for x in l.strip().split()]
-                for l in stream_lines
-                if l.strip()[0] != comment_char]
-        return comments, data
-
-    # Return comment lines number and ordered list of comments and data lines
-    comment_lnum = []
-    data = []
-    for i, l in enumerate(stream_lines):
-        l = l.strip()
-        if l[0] == comment_char:
-            comment_lnum.append(i)
-            data.append(l)
-        else:
-            data.append([f(x) for x in l.split()])
-
-    return comment_lnum, data
+    # Read the input file and filter the bash-like commentss
+    comments = [l.split() for l in tmp_in if l[0] == comment_char]
+    tmp_in = [[f(x) for x in l.split()]
+              for l in tmp_in if l[0] != comment_char]
+    return comments, tmp_in
 
 def load_file(filename, comment_char="#"):
-    """Load given filename.
-
-    Divide lines between comments and data.
-    Comment char can be given as input."""
+    """Load given filename. Divide lines between comments and data.
+    Comment char can be given as input. Wrapped of load_stream function."""
+    # TODO Regex support for multiple comment types.
     with open(filename, 'r') as in_file:
         return load_stream(in_file, comment_char)
-
+    
 def load_float_file(filename, comment_char="#"):
-    """Load given numeric filename.
-
-    Each non-comment field will be cast to float.
+    """Load given numeric filename: each non-comment field will be cast to float. 
     Divide lines between comments and data.
-    Not suitable for big files."""
+    Comment char can be given as input. Wrapped of load_stream function."""
+    # TODO Regex support for multiple comment types.
     with open(filename, 'r') as in_file:
         # Appreciate the power of **kwarg expansion!
         return load_stream(in_file, **{"comment_char" : comment_char, "f": float})
 
-#------------------------------------------------------------------------------#
+#--------------------------------
 # String formatting
-#------------------------------------------------------------------------------#
+#--------------------------------
 
-def set_width(args, w=None, align_char="<", offset=2):
-    """Set the width of given list
-
-    Width can be an int (applied to all args) a list of width (one per argument).
-    If width is not given a the maximum length of the args + offset is used.
-
-    The alignment can be specified giving the proper formatting characher:
-    - "<" for left align (default)
-    - "^" for centred
-    - ">" for right
+def bash_lines2fields_list(line_lst, delim="\s"):
+    """Load filename, read lines and divide into fields.
+    Returns a list of list of fields, i.e. [ [f11,f12,..], [f21,f22,...] ] .
+    Lines starting with # are skipped (assumed as comments)
     """
 
-    # Check aligmnet is valid
-    if align_char not in ["<", "^", ">"]:
-        raise ValueError("Alignment char must be left (<), center (^) or right (>), not",
-                         align_char)
-    # Define template format string
-    tmpl_format = "{:"+align_char+"{width}}"
+    # Skip comments, i.e. lines starting with "#"
+    return [l.split(delim) for l in line_lst
+            if l.strip()[0] != '#']
 
-    # Converte all fields to string. You're never safe enough
-    args = list(map(str, args))
+#def list_fixwidth(in_l, width=None):
+#    """Return a equally width-formatted string from a list.
+# 
+#    Width is give as param or auto-computed from strings length
+#    """
+# 
+#    in_l = lmap(str, in_l) # You're never safe enough
+#    # If not given, width is the max length of the given strings plus 2 (arbitrary)
+#    if width is None:
+#        width = 2+max([len(s) for s in in_l])
+#    # Apply the format to each string
+#    in_l = [field.ljust(width) for field in in_l]
+#    # Join them together and return
+#    return strlist(in_l).strip()
+def list_fixwidth(args, w=None):
+    """Return a constant-width string from a list."""
+    args = list(map(str, args)) # Converte all fields to string. You're never safe enough
+    # If width is not provided, compute a reasonable estimation from arguments.
+    if w is None: w = 2 + max([len(s.strip()) for s in args])
+    # Join the pieces together
+    # Quick-explain: join n formatting command together and sub the provided args (expanded list).
+    # Strip to avoid traling and leading spaces. w-1 bc we join using spaces.
+    return " ".join(["{:<{width}}"]*len(args)).format(*args, width=(w-1)).strip()
 
-    # Width mode: single, list, auto
-    if isinstance(w, int):
-        # --> Single value for all
-        w_args = [w]*len(args)
-    elif isinstance(w, list):
-        # --> Specific width per each arg
-        if False in [isinstance(wi, int) for wi in w]:
-            raise ValueError("Width of each char must be int")
-        if len(w) != len(args):
-            raise ValueError("Width and args must have same length")
-        w_args = w
-    elif w is None:
-        # Auto: compute a reasonable estimation from arguments.
-        w_args = [offset + max([len(s.strip()) for s in args])]*len(args)
-    else:
-        raise ValueError("Width style not recognised")
+def cols_width_fix(rows, width=None):
+    """Apply fixwidth to a nested container.
 
-    # Run over args and relative width, put them in format string and join all.
-    return "".join([tmpl_format.format(s, width=w)
-                    for s, w in zip(args, w_args)]
-                  )
-
-def adjust_col_width(rows, align_char="<", offset=5):
-    """Set width of a n-list of m-list to (max + offset) of each column
-
-    Structure assumed is list of rows: [ [1, 2, ..., m], [1,2,...,m], ..., n]
-    Returns a n-list of  strings.
+    Width is max width of strings in obj.
+    Structure assumed is list of rows: [ [<row1>], [<row2>], ...]
+    Returns a list of fixed-colum-width strings.
     """
 
-    # Get dimension m of the rows (first must be equal to all others)
-    cols_w =  [0]*len(rows[0])
-    # Check input and compute right width
-    for i, ri in enumerate(rows):
-        if len(ri) != len(cols_w):
-            raise ValueError("All lines must have same length")
-        # To be sure, convert to string
-        ri = lmap(str, ri)
-        rows[i] = ri
-        # Loop on fields
-        for j, field in enumerate(ri):
-            w_f = len(field) # Current field width
-            # If longer than current value, update
-            if w_f > cols_w[j]:
-                cols_w[j] = w_f
+    # Flat eventually nested rows and convert to string
+    rows = [lmap(str, lflatten(r)) for r in rows]
+    if width == None:
+        # Find max width amonst all fields. Add arbitrary spacing
+        width = 1+max([len(s.strip()) for s in flatten(rows)])
+    return [list_fix_width(r, width) for r in rows]
 
-    # Apply width offset to computed width and set width of each row
-    return [set_width(r, [w+offset for w in cols_w], align_char=align_char)
-            for r in rows]
+def list_fix_width(args, w=None):
+    """Return a equally width-formatted string from a list.
 
-def set_float_width(args, w=None, align_char="<", offset=2, prec=15):
-    """Set convert list of float to string and set width
-
-    Set precision of conversion with "prec" keyword.
-    Then pass the converted floats to set_width function
+    Width is give as param or auto-computed from strings length.
+    Everything is converted to str using std settings.
     """
 
-    # Converte all fields to string.
-    conv_tmpl= "%"+"%i.%i" % (prec+5, prec)+"f"
-    args = [conv_tmpl % n for n in args ]
-    return set_width(args, w=w, align_char=align_char, offset=offset)
+    args = lmap(str, args) # You're never safe enough
+    # Convert to str, just in case.
+    if w is None:
+        w = 2+max([len(s.strip()) for s in args])
+    return " ".join(["{:<{width}}"]*len(args)).format(*args, width=w).strip()
 
-#------------------------------------------------------------------------------#
-# Lists manipulators
-#------------------------------------------------------------------------------#
+#--------------------------------
+# Flatten nested lists
+#--------------------------------
 
 # Nice recursive, yield-wise flatten function
 def flatten(l):
-    """Flat list generator from nested lists"""
     for el in l:
         if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
             yield from flatten(el)
@@ -211,12 +176,6 @@ def flatten(l):
 
 # Lazy way of do it...
 def lflatten(l):
-    """Flat list from a nested one"""
-    return  list(flatten(l))
+    """Returns a flat list from a nested one"""
 
-def list_uniq(l):
-    """List of uniq elements in given list l.
-    Preserves original order, differently than set."""
-    seen = set()
-    seen_add = seen.add # Faster than evaulating seen.add at each iter
-    return [x for x in l if not (x in seen or seen_add(x))]
+    return  list(flatten(l))
